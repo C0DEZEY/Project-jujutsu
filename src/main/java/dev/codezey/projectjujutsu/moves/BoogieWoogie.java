75940fdd73b07e76535b1328f069bdf1af55cc79
C0DEZEY <melonicholas1113@gmail.com>
2023-10-05 23:59:00 -0400

    Adding Boogie woogie (Targeting)

--- src/main/java/dev/codezey/projectjujutsu/moves/BoogieWoogie.java
+++ src/main/java/dev/codezey/projectjujutsu/moves/BoogieWoogie.java
@@ -1,5 +1,42 @@
 package dev.codezey.projectjujutsu.moves;
 
+import dev.codezey.projectjujutsu.Main;
+import dev.codezey.projectjujutsu.util.raycast;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import dev.codezey.projectjujutsu.util.KeyBinding;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.function.Predicate;
+
 public class BoogieWoogie {
+
+    public void Target(Player player) {
+        Predicate<Entity> entityPredicate = entity -> {
+            return true;
+        };
+
+
+        Entity e = raycast.selectEntityNearCursor(player,50,player.level(),entityPredicate,true);
+    }
+
+    @Mod.EventBusSubscriber(modid = Main.MODID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        // On Keybind (B)
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if(KeyBinding.BW_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Doing shit? "));
+
+
+
+            }
+        }
+    }
 
 }
