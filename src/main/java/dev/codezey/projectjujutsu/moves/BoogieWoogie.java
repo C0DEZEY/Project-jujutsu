052a2a622096700c99523180f94c6fce3c242c66
C0DEZEY <melonicholas1113@gmail.com>
2023-10-09 00:36:35 -0400

    master

--- src/main/java/dev/codezey/projectjujutsu/moves/BoogieWoogie.java
+++ src/main/java/dev/codezey/projectjujutsu/moves/BoogieWoogie.java
@@ -11,9 +11,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import dev.codezey.projectjujutsu.util.KeyBinding;
 import net.minecraft.world.entity.player.Player;
-import dev.codezey.projectjujutsu.util.glow;
-import java.util.function.Predicate;
-import net.minecraft.world.entity.LivingEntity;
+
 public class BoogieWoogie {
 
 
@@ -21,20 +19,18 @@ public class BoogieWoogie {
     @Mod.EventBusSubscriber(modid = Main.MODID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
 
-        public static LivingEntity getEnemy(Player player) {
-            Entity e = raycast.selectEntityNearCursor(player, 5, player.level(), a -> raycast.getPredicate(raycast.MAHOU_SELECTION.SELECTIVE_DISPLACEMENT, a, player), true);
-            if (e instanceof LivingEntity) {
-                return (LivingEntity)e;
-            }
-            return null;}
+        public static boolean getEnemy(Player player) {
+            return raycast.rayTraceEyes(player,50);
+        }
         
         // On Keybind
         @SubscribeEvent
         public static void onKeyInput(InputEvent.Key event) {
             // Do Actions on Keyboard Input
             if(KeyBinding.BW_KEY.consumeClick()) {
-                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Boogie Woogie "));
-                getEnemy(Minecraft.getInstance().player);
+                //Minecraft.getInstance().player.sendSystemMessage(Component.literal("Boogie Woogie "));
+                boolean target = getEnemy(Minecraft.getInstance().player);
+               Minecraft.getInstance().player.sendSystemMessage(Component.literal(String.valueOf(target)));
 
             }
         }
