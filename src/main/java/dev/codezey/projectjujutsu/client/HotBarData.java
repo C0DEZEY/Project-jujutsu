61f2d8b420bd95e98a1eeb07b33a6674baaa492b
Codezey <91379167+C0DEZEY@users.noreply.github.com>
2023-10-20 17:10:48 +0000

    Adding HotBar

--- src/main/java/dev/codezey/projectjujutsu/client/HotBarData.java
+++ src/main/java/dev/codezey/projectjujutsu/client/HotBarData.java
@@ -3,8 +3,22 @@ package dev.codezey.projectjujutsu.client;
 public class HotBarData {
     private static int HotBarSlot = 0;
     private static boolean ActiveHotBar = false; 
+    private static int HotBarSize = 4; 
+    private static int Moves = {0,0,0,0}; // Create a Array of all the moves using ID's 
+ 
 
+
+    public static int ReturnSlot() {
+        return HotBarSlot;
+    }
+
+    public static int ReturnSize() {
+        return HotBarSize;
+    }
+
+    public static boolean ReturnIsActive() {
+        return ActiveHotBar;
+    }
     
-
-
+    
 }
--- src/main/java/dev/codezey/projectjujutsu/client/screens/HotBarOverLay.java
+++ src/main/java/dev/codezey/projectjujutsu/client/screens/HotBarOverLay.java
@@ -1,4 +1,32 @@
 package dev.codezey.projectjujutsu.client.screens;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.Minecraft;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.platform.GlStateManager;
+
+
+@Mod.EventBusSubscriber({Dist.CLIENT})
 public class HotBarOverLay {
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public static void eventHandler(RenderGuiEvent.Pre event) {
+        int w = event.getWindow().getGuiScaledWidth();
+        int h = event.getWindow().getGuiScaledHeight();
+        int posX = w / 2;
+        int posY = h / 2;
+
+        
+
+    }
+
 }
